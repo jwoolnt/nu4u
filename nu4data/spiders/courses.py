7a00cb731980fa926056f30fcb64e1529f9f3277
Jayden Wool <52765228+jwoolnt@users.noreply.github.com>
2024-07-02 23:41:24 -0400

    feat: Update CoursesSpider to Use CourseItem

--- nu4data/spiders/courses.py
+++ nu4data/spiders/courses.py
@@ -1,22 +1,33 @@
-import scrapy
+from scrapy import Spider, Request
+from scrapy.http import HtmlResponse
+
+from nu4data.items import CourseItem
 
 
-class CoursesSpider(scrapy.Spider):
+class CoursesSpider(Spider):
     name = "courses"
-    start_urls = ["https://catalogs.northwestern.edu/undergraduate/courses-az/"]
 
-    def parse(self, response):
+    def start_requests(self):
+        if "production" in dir(self): yield Request("https://catalogs.northwestern.edu/undergraduate/courses-az/")
+        yield Request("https://catalogs.northwestern.edu/undergraduate/courses-az/arabic/", self.parse_subject_page)
+
+    def parse(self, response: HtmlResponse):
         links = response.css(".letternav-head + ul a")
-        yield from response.follow_all(links, self.parse_courses)
 
-    def parse_courses(self, response):
+        yield from response.follow_all(links, self.parse_subject_page)
+
+    def parse_subject_page(self, response: HtmlResponse):
         course_blocks = response.css(".courseblock")
+
         for course_block in course_blocks:
             raw_title = course_block.css(".courseblocktitle strong::text").get()
-            if raw_title is None: continue
-            tidy_title = raw_title.replace("\xa0", " ").strip()
+            title_words = raw_title.split()
+            code = title_words[0] + " " + title_words[1]
+            name = " ".join(title_words[2:-2])
+            units = title_words[-2][-1] # TODO: Add Int Type Checking
 
-            yield {
-                "title": tidy_title,
-                "description": course_block.css(".courseblockdesc::text").get()
-            }
+            description = course_block.css(".courseblockdesc::text").get()
+
+            # TODO: Parse PreReqs
+
+            yield CourseItem(code, name, units, description)
