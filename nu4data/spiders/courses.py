f99882f87b25c638ee3bc623a15cd72ec4caea46
Jayden Wool <52765228+jwoolnt@users.noreply.github.com>
2024-07-03 23:17:11 -0400

    feat: Update CoursesSpider
    Update parse_subject_page & parse_title Functions
    Add parse_prereqs Function

--- nu4data/spiders/courses.py
+++ nu4data/spiders/courses.py
@@ -1,7 +1,9 @@
-from scrapy import Spider, Request
+from re import sub
+
+from scrapy import Spider, Request, Selector
 from scrapy.http import HtmlResponse
 
-from nu4data.items import CourseItem
+from nu4data.items import CourseItem, PreReqs
 
 
 class CoursesSpider(Spider):
@@ -9,7 +11,7 @@ class CoursesSpider(Spider):
 
     def start_requests(self):
         if "production" in dir(self): yield Request("https://catalogs.northwestern.edu/undergraduate/courses-az/")
-        yield Request("https://catalogs.northwestern.edu/undergraduate/courses-az/arabic/", self.parse_subject_page)
+        yield Request("https://catalogs.northwestern.edu/undergraduate/courses-az/afst/", self.parse_subject_page)
 
     def parse(self, response: HtmlResponse):
         links = response.css(".letternav-head + ul a")
@@ -20,25 +22,32 @@ class CoursesSpider(Spider):
         course_blocks = response.css(".courseblock")
 
         for course_block in course_blocks:
-            raw_title = course_block.css(".courseblocktitle strong::text").get()
-            (code, name, units) = self.tidy_title(raw_title)
-
+            (code, name, units) = self.parse_title(course_block)
             description = course_block.css(".courseblockdesc::text").get()
+            prereqs = self.parse_prereqs(course_block)
 
-            # TODO: Parse PreReqs
+            yield CourseItem(code, name, units, description, prereqs)
 
-            yield CourseItem(code, name, units, description)
+    def parse_title(self, course_block: Selector) -> tuple[str, str, int]:
+        words = course_block.css(".courseblocktitle strong::text").get().split()
 
-    def tidy_title(self, raw_title: str) -> tuple[str, str, int]:
-        title_words = raw_title.split()
-
-        code = title_words[0] + " " + title_words[1]
-        name = " ".join(title_words[2:-2])
+        code = words[0] + " " + words[1]
+        name = " ".join(words[2:-2])
         units: int
         try:
-            units = int(title_words[-2][-1])
+            units = int(words[-2][-1])
         except ValueError as error:
             self.logger.error(error)
             units = -1
 
         return (code, name, units)
+
+    def parse_prereqs(self, course_block: Selector) -> PreReqs:
+        html = course_block.css(".courseblockextra").get()
+        if html is None or "Prerequisite" not in html: return None
+
+        tidy_text = sub("<[^>]*>", "", html).strip()
+        tidy_text = sub("\s", " ", tidy_text)
+        tidy_text = tidy_text.removeprefix("Prerequisite: ")
+
+        return tidy_text
